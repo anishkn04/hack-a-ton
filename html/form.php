ee0b36b27378ae040c6c439112b388a2c8972b59
nishan <nishan@gmail.com>
2024-06-29 23:12:04 +0545

    first

--- html/form.php
+++ html/form.php
@@ -1,1 +1,8 @@
 <!-- Nishan Paudel -->
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="sales_database";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+?>
