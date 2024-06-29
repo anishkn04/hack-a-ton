2699fd5384a669bfc9ef141840e1764097d9123d
Rishav Chapagain <gainrishavchap@gmail.com>
2024-06-29 15:49:16 +0545

    Rishav's Initial Commit

--- js/resources.js
+++ js/resources.js
@@ -1,1 +1,9 @@
-// Rishav Chapagain
+let sidebar = document.getElementById("sidebar");
+let sidebar_img = document.getElementById("sidebar_img");
+
+document.getElementById("sidebar_img").addEventListener("click", sidebarOpen);
+
+function sidebarOpen() {
+    sidebar.style.display="flex"
+    sidebar.style.left="0px"
+}
