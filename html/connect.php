d32410d726ade861d86b6e4c2e5c03ee1d12da8d
Rishav Chapagain <gainrishavchap@gmail.com>
2024-07-05 16:11:35 +0545

    Nishan Merge

--- html/connect.php
+++ html/connect.php
@@ -1,11 +1,13 @@
 <?php
-$servername="localhost:3306";
-$username="ODBC";
-$password="";
-$dbname="demo";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
+$env = parse_ini_file('./.env');
+$servername = $env["SERVER"];
+$username = $env["USERNAME"];
+$pass = $env["PASS"];
+$dbname = $env["DBNAME"];
+
+
+$conn=mysqli_connect($servername,$username,$pass,$dbname);
 
 $sql    = "CREATE TABLE IF NOT EXISTS hackathoninfo(id INT AUTO_INCREMENT PRIMARY KEY,sname VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL, phone VARCHAR(20) NOT NULL,saddress VARCHAR(20) NOT NULL,team_details TEXT,title VARCHAR(255),start_dates DATE, skills TEXT,skils TEXT,feedback TEXT,registeredat DATETIME DEFAULT CURRENT_TIMESTAMP())";
 $result = mysqli_query($conn, $sql);
-?>
